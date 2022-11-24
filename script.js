a621f35e15f392dbdc85f29df26b8539428b2f3c
jiyeon <jiyeonpark0024@gmail.com>
2022-11-24 17:52:27 +0900

    third commit

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("hi")
+
+
+const Todos = document.querySelector(".todos");
+const SubmitBtn = document.querySelector(".submitBtn");
+const List= document.querySelector(".listWrap");
+
+
+
+
+SubmitBtn.addEventListener("click",AddTodo);
+// ListWrap.addEventListener("click",TodoLists-btn);
+
+
+
+function AddTodo(e){
+    e.preventDefault();
+
+    // const todoDiv = document.createElement("div");
+    // todoDiv.classList.add("listWrap")
+    const list = document.createElement("li");
+    list.innerText = Todos.value;
+    Todos.value="";
+    list.classList.add("todoList");
+    List.appendChild(list);
+    console.log("btn")
+   
+}
+
